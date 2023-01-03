a072ddc5e634a54fb8265fefa5320136a6bca208
Egbuna Chinedu <egbunachineduvictor@gmail.com>
2023-01-03 10:11:14 +0100

    added media responsiveness

--- js.js
+++ js.js
@@ -3,8 +3,28 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navigation-panel").style.top = "0";
+    document.getElementById('open-btn').style.top = "0"
   } else {
-    document.getElementById("navigation-panel").style.top = "-100px";
+    document.getElementById("navigation-panel").style.top = "-200px";
+    document.getElementById('open-btn').style.top = "-200px"
   }
   prevScrollpos = currentScrollPos;
+}
+
+openBtn = document.getElementById('open-btn');
+closeBtn = document.getElementById('close-btn');
+openBtn.addEventListener('click', openMenu);
+closeBtn.addEventListener('click', closeMenu);
+
+
+function openMenu(){
+  document.querySelector('.side-bar').classList.add('opened')
+
+}
+
+
+
+
+function closeMenu(){
+  document.querySelector('.side-bar').classList.remove('opened')
 }
